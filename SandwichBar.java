5e5d2595afd31d8191b9fd8145094fcdd4ebfa6f
Mariam Sulakian <triplemango@gmail.com>
2016-09-26 10:22:15 -0400

    SandwichBar simplified

--- SandwichBar.java
+++ SandwichBar.java
@@ -1,34 +1,17 @@
 import java.util.Arrays;
 import java.util.HashSet;
 
-public class SandwichBar{
-      public int whichOrder(String[] available, String[] orders){
-    	
-    	//get rid of duplicates in set then change back to array
-    	HashSet<String> availSet = new HashSet<String>(Arrays.asList(available));
-    	available = availSet.toArray(new String[availSet.size()]); //initialize size of array as size of set
-  		
-  		//loop through each order preference
-  		for (int i=0; i<orders.length; i++){
-  			//split each individual order to get ingredients
-  			String[] itemsInOrder = orders[i].split(" ");
-  			int availOrder = 0;
-  			for (String ingred: itemsInOrder){ //loop through all ingredients
-  				for (int y=0; y<available.length; y++){ //loop through available ingredients
-  					if (available[y].equals(ingred)){ //if order ingredient is in available ingredients
-  						availOrder++; //increment num of orders ingredients available
-  					}
-  				}
-  				//OR loop through available this way: 
-//				for(String item: available){
-//					if(item.equals(ingred)){
-//						availOrder++; }
-//				}
-  			}
-  			if (availOrder == itemsInOrder.length){ //if all order ingredients are available
-  				return i; //return index of order
-  			}
-  		}
-  		return -1; //no orders are available
-  		}
-}
+public class SandwichBar {
+	  public int whichOrder(String[] available, String[] orders) {
+	     HashSet<String> bar = new HashSet<>(Arrays.asList(available));
+			
+		int current = 0;
+		for(String s : orders) {
+		   HashSet<String> cs = 
+	                new HashSet<>(Arrays.asList(s.split(" ")));
+		   if (bar.containsAll(cs)) return current;
+		   current += 1;
+		 }
+		 return -1;
+	   }
+	}
