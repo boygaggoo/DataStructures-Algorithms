e83b3238b2953f2989b280e00e1539a7ada0e9b6
Mariam Sulakian <triplemango@gmail.com>
2016-09-26 10:26:08 -0400

    updates

--- UniqueNewYork.java
+++ UniqueNewYork.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class UniqueNewYork {
        public int seen(String[] records) {
     	   HashSet<String> uniqueRecords = new HashSet<String>();
-    	   for(int i=0; i<records.length; i++){
-    		   String[] eachItem = records[i].split(",");
+    	   for(String i : records){
+    		   String[] eachItem = i.split(",");
     		   for(String s : eachItem){
     			   uniqueRecords.add(s);
     		   }
