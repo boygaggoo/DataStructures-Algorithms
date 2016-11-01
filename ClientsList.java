b97ba12eb5ec56fd9027617cf5ec1688525840e7
Mariam Sulakian <triplemango@gmail.com>
2016-11-01 14:21:41 -0400

    comparable, compareTo sorting

--- ClientsList.java
+++ ClientsList.java
@@ -1,4 +1,48 @@
+import java.util.*;
 
 public class ClientsList {
+	public String[] dataCleanup(String[] names) {
+		Person p;
+		String[] ans = new String[names.length];
+		ArrayList<Person> clients = new ArrayList<>();
+		for(int i=0; i<names.length; i++){
+			p = new Person(names[i]);
+			clients.add(p);
+		}
+		Collections.sort(clients); //sort arrayList of clients by name
+		for(int i=0; i<ans.length;i++){
+			ans[i] = clients.get(i).toStr(); //format names
+		}
+		return ans;
+   }
 
+	 public class Person implements Comparable<Person>{
+			String first;
+			String last;
+			
+			//assign correct strings as first and last names
+			public Person(String s) {
+				if(s.contains(",")){
+					first = s.split("\\, ")[1];
+					last = s.split("\\, ")[0];
+				}
+				else{
+				first = s.split(" ")[0];
+				last = s.split(" ")[1];
+				}
+			}
+			//concatenate full name
+			public String toStr(){
+				return first + " " + last;
+			}
+			@Override
+			public int compareTo(Person p) {	
+				//compare by last name
+				int lastComp = (this.last).compareTo(p.last);
+				if(lastComp == 0 ){ //compare by first name
+					return (this.first).compareTo(p.first);
+				}
+				return lastComp;
+			}
+		}
 }
--- Tourney.java
+++ Tourney.java
@@ -1,5 +1,34 @@
+import java.util.*;
 public class Tourney {
       public String winner(String[] bracket, String results) {
-           // fill in code here
+    	  Queue<String> queue = new LinkedList<String>();
+    	  Queue<String> queueRes = new LinkedList<String>();
+    	  
+    	  //add to queue
+    	  for(int i=0;i<bracket.length;i++){
+    		  queue.add(bracket[i]);
+    	  }
+    	  String[] res = results.split(".");
+    	  for(int i=0; i<res.length;i++){
+    		  queueRes.add(res[i]);
+    	  }
+    	  //round 1
+    	  for(int i=0; i<bracket.length-1;i+=2){
+    		  if(bracket[i].equals("bye")||bracket[i+1].equals("bye")){
+    			  if(bracket[i].equals("bye")){
+    				  queue.remove(i);
+    			  }else{
+    				  queue.remove(i+1);
+    				  }
+    		  }else{
+    			  if(res[i].equals("H")){
+    				  queue.remove(i+1);
+    			  }
+    			  if(res[i+1].equals("L")){
+    				  queue.remove(i);
+    			  }
+    			  }
+    		  }
+    	  return results;
       }
    }
