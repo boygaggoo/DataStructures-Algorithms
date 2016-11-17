5eab78694bc892639e3c8c21c01e90065a20cac4
Mariam Sulakian <triplemango@gmail.com>
2016-11-17 14:38:37 -0500

    cleaned up code

--- LeafSum.java
+++ LeafSum.java
@@ -1,27 +1,21 @@
 public class LeafSum {
     public int sum(TreeNode t) {
 	    int total = 0;
-	    if (t == null) {
-	        return total;
-	    }
-	    if(t.left==null && t.right==null){
-	    	return total + t.info;
-	    }
-	    else{
-	    	return sum(t.left) + sum(t.right);
-	    }
+	    if (t == null) return total;
+	    if(t.left==null && t.right==null) return total + t.info;
+	    else return sum(t.left) + sum(t.right);
 	}	
-	public class TreeNode {
-	      int info;
-	      TreeNode left;
-	      TreeNode right;
-	      TreeNode(int x){
-	          info = x;
-	      }
-	      TreeNode(int x, TreeNode lNode, TreeNode rNode){
-	          info = x;
-	   	  left = lNode;
-	          right = rNode;
-	      }
-	  }
+//	public class TreeNode {
+//	      int info;
+//	      TreeNode left;
+//	      TreeNode right;
+//	      TreeNode(int x){
+//	          info = x;
+//	      }
+//	      TreeNode(int x, TreeNode lNode, TreeNode rNode){
+//	          info = x;
+//	   	  left = lNode;
+//	          right = rNode;
+//	      }
+//	  }
 }
