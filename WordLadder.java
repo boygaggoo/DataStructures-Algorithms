56a5be70909cf95348888bf3d6f4e926b9c5c243
Mariam Sulakian <triplemango@gmail.com>
2016-11-17 23:02:04 -0500

    using queue

--- WordLadder.java
+++ WordLadder.java
@@ -2,18 +2,14 @@ import java.util.*;
 
 public class WordLadder {
 	public boolean oneAway(String a, String b){
-		if (a.length() != b.length()){
-			return false;
-		}
+		if (a.length() != b.length()) return false;
 		int diff = 0;
 		for (int i = 0; i < a.length(); i++){
 			if (a.charAt(i) != b.charAt(i)){
 				diff++;
 			}
 		}
-		if (diff == 1){
-			return true;
-		}
+		if (diff == 1) return true;
 		return false;
 	}
 	
@@ -25,14 +21,10 @@ public class WordLadder {
 		 while (qu.size() != 0){
 			 current = qu.remove();
 			 for(String w:words){
-				 if (oneAway(current, w)){
-					 qu.add(w);
-				 }
+				 if (oneAway(current, w)) qu.add(w);
 			 }
-			 if (oneAway(current, to)){
-				 return "ladder";
+			 if (oneAway(current, to)) return "ladder";
 		 }
-}
 		 return "none";
 	 }
 }
